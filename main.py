754ee1fa69a12d96997d7028540f8d5a8ee22f58
Lauren Ferreira <lauren.ferremch@gmail.com>
2019-03-16 18:30:16 -0300

    Implement watermark

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def display_images(repository, files):
         img = cv2.imread(os.path.join(repository, file))
         bordered_img = cv2.copyMakeBorder(
             img, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=BLUE)
+        watermark = cv2.imread("watermark.png")
+        watermark_height, watermark_width = watermark.shape[:2]
+        img_height, img_width = img.shape[:2]
+        bordered_img[img_height-watermark_height:img_height, img_width-watermark_width:img_width] = watermark
         cv2.imshow('img', bordered_img)
         time.sleep(2)
         if cv2.waitKey(1) & 0xFF == ord('q'):
